48b5d297780af5e17c83a088224152e6ad3fa9ff
cjsb <cjsb@cin.ufpe.br>
2016-12-12 15:40:03 -0330

    ray cast working in the scene

--- Code/PathTracerSol/PathTracer/PathTracer.cpp
+++ Code/PathTracerSol/PathTracer/PathTracer.cpp
@@ -2,26 +2,94 @@
 
 
 
-	glm::vec3 tracer(const Ray &ray, const Scene &scene, int &depth){
+glm::vec3 tracer(const Ray &ray, const Scene &scene, int &depth){
 		
-		bool intersected = false;
-		for (auto &obj : scene.objects){
+	bool intersected = false;
+	Intersection inter;
+	intersected = rayCast(ray, scene, inter);
+	/*for (auto &obj : scene.objects){
 
-			Intersection inter;
-			bool find = obj->intersect(ray, inter);
-			if (find){
-				intersected = true;
+		Intersection inter;
+		bool find = obj->intersect(ray, inter);
+		if (find){
+			intersected = true;
 				
-			}
-
 		}
-		if (intersected){
-			return glm::vec3(0, 0, 1);
+
+	}*/
+	if (intersected){
+		if (inter.objType == LIGHT){
+			return glm::vec3(scene.lights.at(inter.index)->r,
+				scene.lights.at(inter.index)->g,
+				scene.lights.at(inter.index)->b);
 		}
 		else{
-			return glm::vec3(1, 1, 1);
+			return glm::vec3(scene.objects.at(inter.index)->material.r,
+				scene.objects.at(inter.index)->material.g,
+				scene.objects.at(inter.index)->material.b);
 		}
 	}
+	else{
+		return glm::vec3(1, 1, 1);
+	}
+}
 
+
+bool rayCast(const Ray &ray, const Scene &scene, Intersection &inter){
+
+	inter.t = std::numeric_limits<double>::max();
+	bool intersected = false;
+	int index = 0;
+	for (auto &obj : scene.objects){
+
+		Intersection interLocal;
+		bool find = obj->intersect(ray, interLocal);
+
+		if (find){
+
+			intersected = true;
+
+			if (interLocal.t < inter.t){
+				inter.t = interLocal.t;
+				inter.worldPosition = interLocal.worldPosition;
+				inter.objType = interLocal.objType;
+				inter.index = index;
+				
+
+			}
+		}
+
+		index++;
+	}
+	index = 0;
+	for (auto &light : scene.lights){
+
+		Intersection interLocal;
+		
+		bool find = light->mesh.intersect(ray, interLocal);
+
+		if (find){
+
+			intersected = true;
+
+			if (interLocal.t < inter.t){
+				inter.t = interLocal.t;
+				inter.worldPosition = interLocal.worldPosition;
+				inter.objType = LIGHT;
+				inter.index = index;
+
+
+			}
+		}
+
+		index++;
+	}
+	
+
+	return intersected;
+
+
+
+}
 
 
--- Code/PathTracerSol/PathTracer/PathTracer.h
+++ Code/PathTracerSol/PathTracer/PathTracer.h
@@ -6,11 +6,13 @@
 #include "Scene.h"
 #include "Ray.h"
 #include "Intersection.h"
+#include "Light.h"
 
 
 
 	glm::vec3 tracer(const Ray &ray, const Scene &scene, int &depth);
 
+	bool rayCast(const Ray &ray, const Scene &scene, Intersection &inter);
 
 
 
