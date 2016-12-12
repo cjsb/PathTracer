4a29fa67a55bc3160777e3f410fc08a74d879199
cjsb <cjsb@cin.ufpe.br>
2016-12-12 08:55:35 -0330

    simples main

--- Code/PathTracerSol/PathTracer/Camera.h
+++ Code/PathTracerSol/PathTracer/Camera.h
@@ -16,8 +16,18 @@ public:
 	glm::vec3 lookAt;
 	glm::vec3 up;
 
+	glm::vec3 cameraDir;
+	glm::vec3 camRight;
+	glm::vec3 camDown;
+
 	Camera(const glm::vec3 eye, const glm::vec3 lookAt, const glm::vec3 up) : zNear(0.1f), zFar(100.f), foV(glm::pi<float>() / 2.0f), eye(eye), lookAt(lookAt), up(up){
+		
+		glm::vec3 diff_btw = lookAt - eye;
+		cameraDir = glm::normalize(diff_btw);
+		camRight = glm::normalize(glm::cross(up, cameraDir));
+		camDown = glm::cross(camRight, cameraDir);
 	};
+	
 	Camera(){};
 
 };
--- Code/PathTracerSol/PathTracer/main.cpp
+++ Code/PathTracerSol/PathTracer/main.cpp
@@ -22,92 +22,35 @@ int main()
 	conf.readOptions("files/cornellroom.sdl", scene);
 	cout << "sdl loaded" << endl;
 	//test ray cast
-	int width = 500;
-	int height = 500;
-	Image img(width, height);
-
-	glm::vec3 O(0, 0, 0);
-	glm::vec3 X(1, 0, 0);
-	glm::vec3 Y(0, -1, 0);
-	glm::vec3 Z(0, 0, 1);
-	
-	glm::vec3 camPos(0, 0, 0);
-	glm::vec3 lookAt(0, 0, -1);
-	glm::vec3 diff_btw = lookAt - camPos;
-	
-	glm::vec3 camdir = glm::normalize(diff_btw); //diff_btw.negative().normalize();
-	glm::vec3 camright = glm::normalize(glm::cross(Y, camdir));
-	glm::vec3 camdown = glm::cross(camright, camdir);
-	Material m;
-	vector<Object*> obj;
-	Sphere sphere(glm::vec3(0, 0, 10), 1);
-
-	Object *quad = new Quadric(1,1,1,0,0,0,0,0,-10,99,m);
-
-	obj.push_back(quad);
-	double u = 0, v = 0;
-	glm::vec3 ver1(10, 10, 40), ver2(-10, 10, 40), ver3(0, -10, 40);
-	Triangle triangle(ver1, ver2, ver3);
-
-	double invWidth = 1 / double(width), invHeight = 1 / double(height);
-	double fov = 30, aspectratio = width / double(height);
-
-	// Trace rays
-	//for (unsigned y = 0; y < height; ++y) {
-	//	for (unsigned x = 0; x < width; ++x) {
-	//		float xx = (2 * ((x + 0.5) * invWidth) - 1) * angle * aspectratio;
-	//		float yy = (1 - 2 * ((y + 0.5) * invHeight)) * angle;
-	//		glm::vec3 raydirX(xx, yy, 1);
-	//		glm::vec3 raydir = glm::normalize(raydirX);
-	//		Ray ray(glm::vec3(0,0,0), raydir);
-	//		double t;
-	//		bool find = quad.intersect(/*cam_ray_origin, cam_ray_direction*/ray, t);
-	//		
-	//		bool findTri = triangle.rayTriangleIntersect(orig, raydir, ver1, ver2, ver3, tri, u, v);
-	//		//cout << t << endl;
-	//		//bool find = 
-	//		if (findTri){
-	//			img.set(x, y, glm::vec3(0, 0, 1));
-	//			//system("pause");
-	//		}
-	//		else{
-	//			img.set(x, y, glm::vec3(1, 1, 1));
-	//		}
-	//	}
-	//}
-
-
-
-
-	int pixel = 0;
-	double xamnt, yamnt;
-	for (int x = 0; x < width; x++) {
-		for (int y = 0; y < height; y++) {
-			pixel = y*width + x;
-
+	Image img(conf.size.x, conf.size.y);
+		
+	for (int x = 0; x < img.width; x++) {
+		for (int y = 0; y < img.height; y++) {
+			double xamnt, yamnt;
 			// start with no anti-aliasing
-			if (width > height) {
+			if (img.width > img.height) {
 				// the image is wider than it is tall
-				xamnt = ((x + 0.5) / width)*img.aspectRatio - (((width - height) / (double)height) / 2);
-				yamnt = ((height - y) + 0.5) / height;
+				xamnt = ((x + 0.5) / img.width)*img.aspectRatio - (((img.width - img.height) / (double)img.height) / 2);
+				yamnt = ((img.height - y) + 0.5) / img.height;
 			}
-			else if (height > width) {
+			else if (img.height > img.width) {
 				// the imager is taller than it is wide
-				xamnt = (x + 0.5) / width;
-				yamnt = (((height - y) + 0.5) / height) / img.aspectRatio - (((height - width) / (double)width) / 2);
+				xamnt = (x + 0.5) / img.width;
+				yamnt = (((img.height - y) + 0.5) / img.height) / img.aspectRatio - (((img.height - img.width) / (double)img.width) / 2);
 			}
 			else {
 				// the image is square
-				xamnt = (x + 0.5) / width;
-				yamnt = ((height - y) + 0.5) / height;
+				xamnt = (x + 0.5) / img.width;
+				yamnt = ((img.height - y) + 0.5) / img.height;
 			}
 
-			glm::vec3 cam_ray_origin = camPos;
+			glm::vec3 cam_ray_origin = scene.camera.eye;
 			//glm::vec3 cam_ray_direction = camdir.vectAdd(camright.vectMult(xamnt - 0.5).vectAdd(camdown.vectMult(yamnt - 0.5))).normalize();
 			double dirX = xamnt - 0.5;
 			double dirY = yamnt - 0.5;
-			glm::vec3 cam_ray_direction = glm::normalize(camdir + ((camright*(float)dirX) + (camdown*(float)dirY)));
-			
+			//glm::vec3 cam_ray_direction = glm::normalize(camdir + ((camright*(float)dirX) + (camdown*(float)dirY)));
+			glm::vec3 cam_ray_direction = glm::normalize(scene.camera.cameraDir + ((scene.camera.camRight*(float)dirX) + (scene.camera.camDown*(float)dirY)));
+
 			Ray ray(cam_ray_origin, cam_ray_direction);
 
 			img.set(x, y, glm::vec3(1, 1, 1));
@@ -124,36 +67,6 @@ int main()
 			}
 			
 
-			//bool foundOne = false;
-			//double t;
-			//for (int i = 0; i < scene.meshes.size(); ++i){
-			//	for (int j = 0; j < scene.meshes.at(i).triangles.size(); ++j){
-			//		bool findTri = scene.meshes.at(i).triangles.at(j).rayTriangleIntersect(ray, t, u, v);
-			//		//cout << t << endl;
-			//		//bool find = 
-			//		if (findTri){
-			//			img.set(x, y, glm::vec3(0, 0, 1));
-			//			foundOne = true;
-			//		}
-			//	}
-
-			//}
-			//if (!foundOne){
-			//	img.set(x, y, glm::vec3(1, 1, 1));
-			//}
-
-			//Intersection inter;
-			//auto obj1 = obj.at(0);
-			//bool find = obj1->intersect(/*cam_ray_origin, cam_ray_direction*/ray, inter);
-			////bool findTri = triangle.rayTriangleIntersect(ray, t, u, v);
-			////cout << t << endl;
-			////bool find = 
-			//if (find){
-			//	img.set(x, y, glm::vec3(0, 0, 1));
-			//}
-			//else{
-			//	img.set(x, y, glm::vec3(1, 1, 1));
-			//}
 		}
 	}
 	img.save("testeImage.ppm"); 
