f92fa19790a5b935bd6a3f887c00485362c2b1a9
cjsb <cjsb@cin.ufpe.br>
2016-12-15 10:35:15 -0300

    first attempt quadric

--- Code/PathTracerSol/PathTracer/Quadric.h
+++ Code/PathTracerSol/PathTracer/Quadric.h
@@ -27,11 +27,11 @@ public:
 
 	double  a, b, c, d, e;	// Surface coefficents
 	double  f, g, h, j, k;
-	Material material;				// Surface's material properties
+	//Material material;				// Surface's material properties
 
 	Quadric(const double& a, const double& b, const double& c, const double& d, const double& e, const double& f, const double& g, const double& h,
-		const double& j, const double& k, const Material& material)
-		: a(a), b(b), c(c), d(d), e(e), f(f), g(g), h(h), j(j), k(k), material(material) {};
+		const double& j, const double& k)
+		: a(a), b(b), c(c), d(d), e(e), f(f), g(g), h(h), j(j), k(k) {};
 	
 	Quadric(){};
 	~Quadric(){};
